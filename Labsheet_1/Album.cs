34c693c862cf6fe8abeccded6223b99d4d7303d2
Harry Flanagan <harryflanagan756@gmail.com>
2020-01-26 22:00:20 +0000

    Filled Albums list box

---
+++ Labsheet_1/Album.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet_1
+{
+    class Album
+    {
+   
+
+        //Properties
+        public string AlbumName { get; set; }
+        public int Released { get; set; }
+        public int Sales { get; set; }
+
+        public int RandomNumber()
+        {
+            Random random = new Random();
+            return random.Next(1990, 2020);
+        }
+
+        //Constructor
+        public Album(string albumName, int sales)
+        {
+            AlbumName = albumName;
+
+            Released = RandomNumber();
+
+            Sales = sales;
+
+        }
+
+        public override string ToString()
+        {
+
+            return $"{AlbumName} Relased: {Released} Sales: {Sales}";
+        }
+
+    }
+}
--- Labsheet_1/MainWindow.xaml.cs
+++ Labsheet_1/MainWindow.xaml.cs
@@ -26,6 +26,15 @@ namespace Labsheet_1
         List<Band> allPopBands = new List<Band>();
         List<Band> selectedBand = new List<Band>();
 
+        List<Album> amAlbum = new List<Album>();
+        List<Album> tiAlbum = new List<Album>();
+        List<Album> vwAlbum = new List<Album>();
+        List<Album> pAlbum = new List<Album>();
+        List<Album> rocAlbum = new List<Album>();
+        List<Album> mdAlbum = new List<Album>();
+
+        List<Band> selectedAlbum = new List<Band>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -38,7 +47,19 @@ namespace Labsheet_1
             IndieBand b3 = new IndieBand("Vampire Weekend", 2002, "Alex Turner, Jamie Cook, Matt Melders, Nick Allbrook, Nick O'Malley");
             RockBand b4 = new RockBand("Peace", 2009, "Harrison Koisser, Samuel Koisser, Dominic Boyce, Douglas Castle");
             PopBand b5 = new PopBand("Rex Orange County", 2016, "Alex O'Connor");
-            IndieBand b6 = new IndieBand("Mac Demarco", 2012, "Mac Demarco");
+            IndieBand b6 = new IndieBand("Mac DeMarco", 2012, "Mac Demarco");
+
+            //Create Albums
+            Album am1 = new Album("AM", 200000);
+            Album am2 = new Album("Favourite Worst Nightmare", 10000);
+            Album ti1 = new Album("Currents", 15000);
+            Album vw1 = new Album("Dianne", 30000);
+            Album p1 = new Album("Happy People", 4000);
+            Album p2 = new Album("In Love", 4000);
+            Album roc1 = new Album("Bcos U Will Never B Free", 34000);
+            Album roc2 = new Album("Pony", 9900);
+            Album md1 = new Album("Here Comes the Cowboy", 16700);
+            Album md2 = new Album("Salad Days", 943000);
 
             //Insert Bands into list//
             allBands.Add(b1);
@@ -48,6 +69,18 @@ namespace Labsheet_1
             allBands.Add(b5);
             allBands.Add(b6);
 
+
+            //Insert Albums into their corrisponding bands List//
+            amAlbum.Add(am1);
+            amAlbum.Add(am2);
+            tiAlbum.Add(ti1);
+            vwAlbum.Add(vw1);
+            pAlbum.Add(p1);
+            pAlbum.Add(p2);
+            rocAlbum.Add(roc1);
+            rocAlbum.Add(roc2);
+            mdAlbum.Add(md1);
+            mdAlbum.Add(md2);
 
             /*Ask Keith how to replace with a foreach loop*/
 
@@ -100,9 +133,41 @@ namespace Labsheet_1
                 //Display band memebers in text box
                 txtBandMemebrs.Text = ("Members : " + selectedBand.Members.ToString());
 
+                //Display the bands album list
+                switch(selectedBand.BandName)
+                {
+                    case "Arctic Monkys":
+                        lbxAlbums.ItemsSource = null;
+                        lbxAlbums.ItemsSource = amAlbum;
+                        break;
+                    case "Tame Implala":
+                        lbxAlbums.ItemsSource = null;
+                        lbxAlbums.ItemsSource = tiAlbum;
+                        break;
+                    case "Vampire Weekend":
+                        lbxAlbums.ItemsSource = null;
+                        lbxAlbums.ItemsSource = vwAlbum;
+                        break;
+                    case "Peace":
+                        lbxAlbums.ItemsSource = null;
+                        lbxAlbums.ItemsSource = pAlbum;
+                        break;
+                    case "Rex Orange County":
+                        lbxAlbums.ItemsSource = null;
+                        lbxAlbums.ItemsSource = rocAlbum;
+                        break;
+                    case "Mac DeMarco":
+                        lbxAlbums.ItemsSource = null;
+                        lbxAlbums.ItemsSource = mdAlbum;
+                        break;
+                }
+
+      
 
             }
 
         }
+
+      
     }
 }
